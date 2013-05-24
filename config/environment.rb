b2c44ee6e165b6c0bb77ac43effe0219bb1c72df
Marc Boanas <marcjohnboanas@gmail.com>
2013-05-24 22:02:16 +0100

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['App15469480@heroku.com'],
-    :password       => ENV['a9rbsldu'],
+    :user_name      => 'App15469480@heroku.com',
+    :password       => 'a9rbsldu',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
 }
