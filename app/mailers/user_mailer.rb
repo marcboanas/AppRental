d6b341b88ff40207e1f17fd61140efa8d2fd1ed9
Marc Boanas <marcjohnboanas@gmail.com>
2013-05-24 21:44:08 +0100

    commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
     default :from => "enquiries@thompsonsrentals.co.uk"
     
-    def registration_confirmation(user)
+    def registration_confirmation(contact)
         @contact = contact
         mail(:to => "#{contact.name} <#{contact.email}>", :subject => "Registered")
     end
